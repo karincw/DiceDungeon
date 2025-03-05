c9aa0a6072ef642f44ceba6ed8e78436c6be79de
Yeok11 <sin2gami@naver.com>
2025-03-05 09:20:06 +0700

    Dice Mover

--- DiceDungeon/Assets/01.Scripts/Shy/Dice.cs
+++ DiceDungeon/Assets/01.Scripts/Shy/Dice.cs
@@ -4,55 +4,73 @@ using DG.Tweening;
 
 namespace SHY
 {
-    public class Dice : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IDragHandler, IDropHandler, IEndDragHandler
+    public class Dice : MonoBehaviour, IPointerClickHandler, 
+        IDragHandler, IBeginDragHandler, IEndDragHandler
     {
         private Vector2 clickPos;
         private bool isDrager = false;
-        private GameObject changeDice;
+        private int sibleIdx;
+
+        private Sequence seq;
+
+        private void Awake()
+        {
+            seq = DOTween.Sequence();
+        }
+
+        private bool ACheck(Transform _trm) => _trm.GetChild(0).gameObject.activeSelf;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (isDrager) return;
+
+            transform.GetChild(0).gameObject.SetActive(!ACheck(transform));
+        }
+
+
+        #region Dice Move
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            clickPos = eventData.position;
+
+            isDrager = true;
+
+            if (ACheck(transform)) return;
+            sibleIdx = transform.parent.GetSiblingIndex();
+            transform.parent.SetAsLastSibling();
+        }
 
         public void OnDrag(PointerEventData eventData)
         {
-            isDrager = true;
+            if (!isDrager || ACheck(transform)) return;
 
             transform.localPosition += new Vector3(eventData.position.x - clickPos.x, eventData.position.y - clickPos.y);
             clickPos = eventData.position;
-
-            //if(Vector2.Distance(transform.position, changeDice.transform.position) > 200)
-            //{
-            //    Debug.Log("돌아와라!");
-            //}
-        }
-
-        public void OnDrop(PointerEventData eventData)
-        {
-
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
+            transform.DOLocalMove(Vector2.zero, 0.1f);
+
+            if (ACheck(transform)) return;
+            transform.parent.SetSiblingIndex(sibleIdx);
             isDrager = false;
-        }
-
-        public void OnPointerClick(PointerEventData eventData)
-        {
-            //주사위 선택
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            clickPos = eventData.position;
         }
 
         private void OnTriggerEnter2D(Collider2D _col)
         {
-            Debug.Log("넣었다" + transform.parent.gameObject.name);
+            if (!isDrager || ACheck(_col.transform)) return;
 
-            if (!isDrager) return;
+            Transform newParent = _col.transform.parent;
+            _col.transform.SetParent(transform.parent);
+            transform.SetParent(newParent);
 
-            Debug.Log("깊숙히");
+            seq.Append(_col.transform.DOLocalMove(Vector3.zero, 0.1f));
 
-            changeDice = _col.transform.parent.gameObject;
-            _col.transform.DOMove(transform.parent.position, 0.3f);
+            _col.transform.parent.SetSiblingIndex(sibleIdx);
+            sibleIdx = transform.parent.GetSiblingIndex();
+            transform.parent.SetAsLastSibling();
         }
+        #endregion
     }
 }
