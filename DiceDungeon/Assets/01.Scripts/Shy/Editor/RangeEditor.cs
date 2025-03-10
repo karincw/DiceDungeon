7a9802952cd26b26038770db254863b1cee0238a
unknown <sin2gami@naver.com>
2025-03-10 20:29:37 +0700

    .

--- DiceDungeon/Assets/01.Scripts/Shy/Editor/RangeEditor.cs
+++ DiceDungeon/Assets/01.Scripts/Shy/Editor/RangeEditor.cs
@@ -24,13 +24,13 @@ public class RangeEditor : Editor
             for (int x = 0; x < arrX[y]; x++)
             {
                 GUILayout.Space(5);
-                bool oldValue = range.arr[y, x]; // 기존 값 저장
+                bool oldValue = range.arr[y, x];
                 bool newValue = EditorGUILayout.Toggle(oldValue, GUILayout.Width(10));
 
-                if (oldValue != newValue) // 값이 변경된 경우
+                if (oldValue != newValue)
                 {
                     range.arr[y, x] = newValue;
-                    EditorUtility.SetDirty(target); // 변경 사항 저장
+                    EditorUtility.SetDirty(target);
                 }
             }
 
